5902c63d83f327fa710e5e4a93ff7e5bee3ed061
CaiqueROC <caique_ramos2010@hotmail.com>
2024-11-14 15:54:00 -0300

    adicionando mais mangás

--- src/Mock/Data.ts
+++ src/Mock/Data.ts
@@ -4,9 +4,9 @@ import Img3 from "./images/Lookism.png"
 import Img4 from "./images/SoloLevelingRagnarok.png"
 import Img5 from "./images/ManagerKim.jpeg"
 import Img6 from "./images/MercenaryEnrollment.png"
-import Img7 from "./images/WindBreaker.png"
-import Img8 from "./images/StudyGroup.png"
-import Img9 from "./images/InfiniteMage.png"
+import Img7 from "./images/DBS.png"
+import Img8 from "./images/Naruto.png"
+import Img9 from "./images/OnePiece.png"
 import Img10 from "./images/GodOfBlackfield.png"
 import Img11 from "./images/HGAYM.png"
 import Img12 from "./images/MrBaek.png"
@@ -16,6 +16,9 @@ import Img15 from "./images/Overgeared.png"
 import Img16 from "./images/KillerPeter.png"
 import Img17 from "./images/LogIntoTheFuture.png"
 import Img18 from "./images/ORV.png"
+import Img19 from "./images/WindBreaker.png"
+import Img20 from "./images/StudyGroup.png"
+import Img21 from "./images/InfiniteMage.png"
 
 
 
@@ -26,9 +29,9 @@ export const mangas = [
     { id: '4', title: 'Solo Levelin: Ragnarok', chapter: '28', image: Img4 },
     { id: '5', title: 'Manager Kim', chapter: '164', image: Img5 },
     { id: '6', title: 'Mercenary Enrollment', chapter: '213', image: Img6 },
-    { id: '7', title: 'Wind Breaker', chapter: '552', image: Img7 },
-    { id: '8', title: 'Study Group', chapter: '250', image: Img8 },
-    { id: '9', title: 'Infinite Mage', chapter: '92', image: Img9},
+    { id: '7', title: 'Dragon Ball Super', chapter: '113', image: Img7},
+    { id: '8', title: 'Naruto', chapter: '700', image: Img8 },
+    { id: '9', title: 'One Piece', chapter: '1131', image: Img9 },
     { id: '10', title: 'God of Blackfield', chapter: '202', image: Img10 },
     { id: '11', title: 'Heavenly Grand Archives Young Master', chapter: '102', image: Img11 },
     { id: '12', title: 'Mr. Baek', chapter: '101', image: Img12 },
@@ -38,4 +41,7 @@ export const mangas = [
     { id: '16', title: 'Killer Pietro', chapter: '65', image: Img16 },
     { id: '17', title: 'Log Into The Future', chapter: '168', image: Img17 },
     { id: '18', title: 'Omniscient Readers Viewpoint', chapter: '236', image: Img18 },
+    { id: '19', title: 'Wind Breaker', chapter: '552', image: Img19 },
+    { id: '20', title: 'Study Group', chapter: '250', image: Img20},
+    { id: '21', title: 'Infinite Mage', chapter: '92', image: Img21},
   ];
